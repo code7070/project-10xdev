31189d92a33b6180dca512afbdb6aac512383405
Jojo <aabdul3@pmintl.net>
2024-12-03 00:33:37 +0700

    fixing routes

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -1,16 +1,18 @@
 import NavigationSide from "@/components/navigation";
 import ClientLayout from "./client-layout";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function AppLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const cookie = await cookies();
-
   const token = cookie.get("pms-token")?.value;
 
-  if (!token) redirect("/login");
+  const headersList = await headers();
+  const pathname = headersList.get("x-invoke-path") || "/";
+
+  if (!token && pathname !== "/") redirect("/login");
 
   return (
     <main className="flex">
