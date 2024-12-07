7a1c45b4d67fd4b6a13788826a3b3083d1a26f36
Jojo <aabdul3@pmintl.net>
2024-12-08 05:06:04 +0700

    fixing

--- frontend/src/components/teams/list.tsx
+++ frontend/src/components/teams/list.tsx
@@ -53,25 +53,9 @@ export default function TeamsList() {
       {isPending ? (
         <Loader className="size-14 animate-spin" />
       ) : (
-        <div
-          variants={{
-            hidden: { opacity: 0 },
-            visible: {
-              opacity: 1,
-              transition: { delayChildren: 0.2, staggerChildren: 0.25 },
-            },
-          }}
-          initial="hidden"
-          animate="visible"
-          exit="hidden"
-          className="grid grid-cols-2 lg:grid-cols-4 gap-4"
-        >
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
           {list.map((i) => (
             <div
-              variants={{
-                initial: { opacity: 0, scale: 0.8, y: 5 },
-                visible: { opacity: 1, scale: 1, y: 0 },
-              }}
               key={i.login.uuid}
               className="bg-gray-100 p-3 border-2 border-b-gray-300 rounded-app-radius flex gap-2"
             >
